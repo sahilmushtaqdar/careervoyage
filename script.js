3d4d541dc612cf7830bf7cfc78285eb2c29a8f21
sahilmushtaqdar <sdar74694@gmail.com>
2026-07-23 11:57:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     alert("Welcome to CareerVoyage! Explore your dream career.");
 });
+function searchCourse() {
+    let search = document.getElementById("searchInput").value.toLowerCase();
+
+    if (search === "tourism") {
+        alert("Tourism & Hospitality course is available!");
+    } else if (search === "technology") {
+        alert("Technology course is available!");
+    } else if (search === "business") {
+        alert("Business Management course is available!");
+    } else if (search === "web development") {
+        alert("Web Development course is available!");
+    } else {
+        alert("Sorry! Course not found.");
+    }
+}
